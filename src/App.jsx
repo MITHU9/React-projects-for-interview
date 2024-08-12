2c2cced68a597116c3c010e3429be528ab3037db
Shahariar Mithu <kmmithu2015@gmail.com>
2024-08-13 00:50:52 +0600

    star rating project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Index from "./components/accordian";
 import RandomColor from "./components/random-color/RandomColor";
+import Rating from "./components/star-rating/Rating";
 
 function App() {
   return (
     <div className="app">
       <Index />
       <RandomColor />
+      <Rating noOfStars={10} />
     </div>
   );
 }
---
+++ src/components/star-rating/Rating.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./styles.css";
+
+const Rating = ({ noOfStars = 5 }) => {
+  const [hoveredStar, setHoveredStar] = useState(0);
+  const [selectedStar, setSelectedStar] = useState(0);
+
+  const handleClick = (index) => {
+    setSelectedStar(index);
+  };
+  const handleMouseEnter = (index) => {
+    setHoveredStar(index);
+  };
+  const handleMouseLeave = () => {
+    setHoveredStar(selectedStar);
+  };
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+
+        return (
+          <FaStar
+            key={index}
+            className={
+              index <= (selectedStar || hoveredStar) ? "active" : "disabled"
+            }
+            onClick={() => handleClick(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={30}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Rating;
